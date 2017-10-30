e881fcbbb614dc178142b568a20a5316a841fce2
Federico Knüssel <fknussel@gmail.com>
2017-10-30 15:29:27 -0300

    Add new response format

--- index.php
+++ index.php
@@ -1,64 +1,149 @@
 <?php
 
-header('Content-type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: X-Requested-With');
+// ----------------------------------------------------
+// CORS: Allow requests from all origins
+// ----------------------------------------------------
+header("Content-type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 // ----------------------------------------------------
 // Hit the OpenWeatherMap API
 // ----------------------------------------------------
-$owm_api_key = 'df44f0116a96d066cf9aba38b715486b';
-$owm_endpoint = 'https://api.openweathermap.org/data/2.5/' .
-  'weather?q=Freyre,Cordoba&units=metric&lang=es&appid=' . $owm_api_key;
+$owm_api_key = "df44f0116a96d066cf9aba38b715486b";
+$owm_endpoint = "https://api.openweathermap.org/data/2.5/" .
+  "weather?q=Freyre,Cordoba&units=metric&lang=es&appid=" . $owm_api_key;
 $owm_response = file_get_contents($owm_endpoint);
 $owm_data = json_decode($owm_response, TRUE);
 
 // ----------------------------------------------------
 // Hit the Local Weather Station API
 // ----------------------------------------------------
-$local_station_endpoint = 'http://magya.omixom.com/xmlDatosEstaciones.php';
-$local_station_response = file_get_contents($local_station_endpoint);
+$local_station_endpoint = "http://magya.omixom.com/xmlDatosEstaciones.php";
+$local_station_response = file_get_contents($local_station_endpoint); // XML response
 
-// Parse XML to JSON
+// Parse XML to JSON.
 $local_station_xml_data = simplexml_load_string($local_station_response);
 $local_station_json_data = json_decode(json_encode($local_station_xml_data), TRUE);
 
-// Grab weather stations
-$stations = $local_station_json_data['RedEstaciones']['Estacion'];
+// Grab data from all weather stations.
+$all_stations = $local_station_json_data["RedEstaciones"]["Estacion"];
 
-// From all stations, filter out the one with ID 30128 (Freyre)
-$freyre = array_filter($stations, function ($station) {
-  return $station['numero'] == '30128';
+// From all stations, filter out the one with ID 30128 (Freyre).
+// This returns an array with a single element.
+$local_station_data_array = array_filter($all_stations, function ($station) {
+  return $station["numero"] == "30128";
 });
 
-// Get the weather data from the station
-$weather = array_values($freyre)[0];
+// Return a flattened array of weather data.
+$local_station_data = array_values($local_station_data_array)[0];
 
-// Grab missing data from OpenWeatherMap
-$weather['presion'] = $owm_data['main']['pressure'] . ' hPa';
-$weather['altura'] = $owm_data['main']['sea_level'] . ' msnm';
-$weather['imagen'] = array(
-  'id' => $owm_data['weather'][0]['id'],
-  'tipo' => $owm_data['weather'][0]['main'],
-  'descripcion' => $owm_data['weather'][0]['description'],
-  'icono' => $owm_data['weather'][0]['icon'],
-  'url' => 'https://' . $_SERVER['SERVER_NAME'] . '/iconos/' . $owm_data['weather'][0]['icon'] . '.svg'
+// ----------------------------------------------------
+// Response metadata
+// ----------------------------------------------------
+$meta = array(
+  "code" => 200,
+  "message" => "OK"
 );
 
-// Add units
-$weather['temperatura'] = $weather['temperatura'] . '°';
-$weather['temperaturaMaxima'] = $weather['temperaturaMaxima'] . '°';
-$weather['temperaturaMinima'] = $weather['temperaturaMinima'] . '°';
-$weather['sensacionTermica'] = $weather['sensacionTermica'] . '°';
-$weather['humedad'] = $weather['humedad'] . '%';
-$weather['puntoRocio'] = $weather['puntoRocio'] . '°';
-$weather['velocidadViento'] = $weather['velocidadViento'] . ' km/h';
-$weather['precipitacionDia'] = $weather['precipitacionDia'] . ' mm';
-$weather['precipitacionSemana'] = $weather['precipitacionSemana'] . ' mm';
-$weather['precipitacionMes'] = $weather['precipitacionMes'] . ' mm';
+// ----------------------------------------------------
+// Parse response data
+// ----------------------------------------------------
+$data = array(
+  "updated_at" => $local_station_data["fechaMuestra"],
+  "location" => array(
+    "name" => "Freyre, Córdoba, Argentina",
+    "latitude" => floatval($local_station_data["latitud"]),
+    "longitude" => floatval($local_station_data["longitud"]),
+    "altitude" => $owm_data["main"]["sea_level"]
+  ),
+  "temperature" => array(
+    "current" => array(
+      "label" => "Temperatura actual",
+      "value" => floatval($local_station_data["temperatura"]),
+      "unit" => "°C"
+    ),
+    "max" => array(
+      "label" => "Temperatura máxima registrada hoy",
+      "value" => floatval($local_station_data["temperaturaMaxima"]),
+      "unit" => "°C"
+    ),
+    "min" => array(
+      "label" => "Temperatura mínima registrada hoy",
+      "value" => floatval($local_station_data["temperaturaMinima"]),
+      "unit" => "°C"
+    ),
+    "feelsLike" => array(
+      "label" => "Sensación térmica",
+      "value" => floatval($local_station_data["sensacionTermica"]),
+      "unit" => "°C"
+    )
+  ),
+  "humidity" => array(
+    "label" => "Humedad porcentual",
+    "value" => floatval($local_station_data["humidity"]),
+    "unit" => "%"
+  ),
+  "pressure" => array(
+    "label" => "Presión atmosférica",
+    "value" => $owm_data["main"]["pressure"],
+    "unit" => "hPa"
+  ),
+  "dewPoint" => array(
+    "label" => "Punto de rocío",
+    "value" => floatval($local_station_data["puntoRocio"]),
+    "unit" => "°C"
+  ),
+  "wind" => array(
+    "speed" => array(
+      "label" => "Velocidad del viento",
+      "value" => floatval($local_station_data["velocidadViento"]),
+      "unit" => "km/h"
+    ),
+    "direction" => array(
+      "label" => "Dirección del viento",
+      "value" => $local_station_data["direccionViento"],
+      "unit" => null
+    )
+  ),
+  "cummulativeRain" => array(
+    "day" => array(
+      "label" => "Precipitación acumulada hoy",
+      "value" => floatval($local_station_data["precipitacionDia"]),
+      "unit" => "mm"
+    ),
+    "week" => array(
+      "label" => "Precipitación acumulada esta semana",
+      "value" => floatval($local_station_data["precipitacionSemana"]),
+      "unit" => "mm"
+    ),
+    "month" => array(
+      "label" => "Precipitación acumulada en el mes en curso",
+      "value" => floatval($local_station_data["precipitacionMes"]),
+      "unit" => "mm"
+    ),
+    "year" => array(
+      "label" => "Precipitación acumulada en el transcurso del año",
+      "value" => null,
+      "unit" => "mm"
+    )
+  ),
+  "icon" => array(
+    "id" => $owm_data["weather"][0]["id"],
+    "main" => $owm_data["weather"][0]["main"],
+    "description" => $owm_data["weather"][0]["description"],
+    "filename" => $owm_data["weather"][0]["icon"],
+    "url" => "//" . $_SERVER["SERVER_NAME"] . "/icons/" . $owm_data["weather"][0]["icon"] . ".svg"
+  )
+);
 
+// ----------------------------------------------------
 // Return the data as a JSON object
-$json_response = json_encode($weather);
+// ---------------------------------------------------
+$json_response = json_encode(array(
+  "meta" => $meta,
+  "data" => $data
+));
 
 echo $json_response;
